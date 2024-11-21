a2f7f390cf18a87a4819b76b41d8f1be8766303c
piy-771 <piyushsahu2103@gmail.com>
2024-11-21 23:11:18 +0530

    "Employee Program"

---
+++ src/ProgramUsingList.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+class Employee{
+    private  int employeeId;
+    private  String name;
+    private  double salary;
+
+    public Employee(int employeeId, String name, double salary) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+    public void print(){
+        System.out.println("Employee Id = "+getEmployeeId());
+        System.out.println("Employee Name = " + getName());
+        System.out.println("Employee Salary = " + getSalary());
+    }
+}
+
+public class ProgramUsingList {
+    public static void main(String[] args) {
+
+        List<Employee> list = new ArrayList<>();
+
+        list.add(new Employee(123,"Lily",40000));
+        list.add(new Employee(124,"Jiya",50000));
+        list.add(new Employee(125,"Piyush",80000));
+
+        Iterator i = list.iterator();
+        while (i.hasNext()){
+            Employee employee = (Employee) i.next();
+            employee.print();
+        }
+
+
+    }
+}
